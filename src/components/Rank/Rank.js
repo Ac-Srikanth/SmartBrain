cfe4dd95e1485ee25592924311c7c23d1ba8fae8
Srikanth Ac <srikanthac24@gmail.com>
2019-12-13 21:17:13 +0530

    Add all Components

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+	return (
+		<div>
+			<div className='white f3'>
+				<p>Srikanth your current rank is....</p>
+			</div>
+			<div className='white f2'>
+				<p>#5</p>
+			</div>
+		</div>
+	);
+};
+
+export default Rank;
